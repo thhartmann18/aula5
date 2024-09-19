8f9761ef5b9bbaba616f7e67162dadf9202e1dc6
Th <thiago.r.hartmann@gmail.com>
2024-09-19 11:54:31 -0300

    aula 5 exercicios

---
+++ index.js
@@ -0,0 +1,14 @@
+const cachorros =["pincher", "rotviler", "pudol", "pitbul"]
+const numero =prompt("enserir o numero de 0 a 4")
+
+console.log (cachorros[numero])
+
+const numeros=[1,2,3,4,5,6]
+console.log(numeros.length)
+numeros.push(7)
+console.log(numeros)
+numeros.splice(4,5)
+console.log(numeros)
+console.log(numeros.length)
+
+
